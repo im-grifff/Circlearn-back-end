3da6cc527e0536f2a3186dcfd94ae8f82b206af4
Sitouxz <yehezkielowen0901@gmail.com>
2023-10-18 00:44:32 +0800

    Change room data type into array

--- src/model/runding.js
+++ src/model/runding.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 //skema data dari database mongodb (dibutuhkan untuk melakukan crud dengan mongoose)
 const RundingSchema = new mongoose.Schema(
@@ -6,19 +6,21 @@ const RundingSchema = new mongoose.Schema(
     logo_grup: { type: String, required: true },
     subject: { type: String, required: true },
     deskripsi: { type: String, required: true },
-    jenisRunding: { type: String, required: true },
-    administrator: [{ type: mongoose.Schema.Types.ObjectId, ref: "UserSchema" }],
-    admin_username: [{type: String}],
-    peserta: [{ type: mongoose.Schema.Types.ObjectId, ref: "UserSchema" }],
-    meetDateStart : { type: Date },
-    meetDateEnd : { type: Date },
+    jenisRunding: { type: Array, required: true },
+    administrator: [
+      { type: mongoose.Schema.Types.ObjectId, ref: 'UserSchema' }
+    ],
+    admin_username: [{ type: String }],
+    peserta: [{ type: mongoose.Schema.Types.ObjectId, ref: 'UserSchema' }],
+    meetDateStart: { type: Date },
+    meetDateEnd: { type: Date },
     meetLink: { type: String },
-    meetTime: { type: String },
+    meetTime: { type: String }
   },
-  { collection: "rundings", timestamps: true }
+  { collection: 'rundings', timestamps: true }
 );
 //jenisRunding meliputi : Sains, Teknologi, Programming, Agrikultur, Bisnis, Kesehatan, Debat, Hiburan, Kuliner, Olahraga, Other
 
-const Runding = mongoose.model("RundingSchema", RundingSchema);
+const Runding = mongoose.model('RundingSchema', RundingSchema);
 
 module.exports = Runding;
